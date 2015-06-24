b57481da974f98285fc2cd5bfc8e71d59a98da7f
longthan01 <longthan385922@gmail.com>
2015-06-24 22:33:19 +0700

    DistributorWindow.xaml
    DistributorWindow.xaml

---
+++ Unilever/Distributor/DistributorWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Ribbon;
+
+
+namespace Unilever.Distributor
+{
+    /// <summary>
+    /// Interaction logic for DistributorWindow.xaml
+    /// </summary>
+    public partial class DistributorWindow : DXRibbonWindow
+    {
+        public DistributorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnViewPros_ItemClick_1(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void btnViewCats_ItemClick_1(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void btnAddOrder_ItemClick_1(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void btnAddSalemans_ItemClick_1(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void btnSaleRevenue_ItemClick_1(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void btnManageInventory_ItemClick_1(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+
+        }
+    }
+}
